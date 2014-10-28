c300f706dcf64f2c814b7ce25565964af8daa8be
nithinvnath <vnath.nithin@gmail.com>
2014-10-29 00:46:11 +0530

    Implemented flush page and read page
    TODO: Move 'int to byte array' and vice-versa in separate function

---
+++ physical/buffermanager.c
@@ -0,0 +1,18 @@
+/*
+ * buffermanager.c
+ *
+ *  Created on: 28-Oct-2014
+ *      Author: nithin
+ */
+
+#include "../include/globals.h"
+
+/**
+ * Sets the page content in a buffer
+ *
+ * @param relNum
+ * @param contents
+ */
+void setPageContent(int relNum, char *contents){
+    strcpy()
+}
--- physical/flushpage.c
+++ physical/flushpage.c
@@ -1,6 +1,43 @@
-#include "globals.h"
+#include <string.h>
+#include "../include/globals.h"
+#include "../include/filewrapper.h"
+#include "../include/error.h"
 
-FlushPage(int relNum)
-{
+/**
+ * Flush the open relation's buffer pool page. This involves writing the page to disk if it's
+ * dirty, then marking the buffer page as being clean.
+ *
+ * @param relNum - the relation number
+ * @author nithin
+ */
 
+//TODO Test
+FlushPage(int relNum) {
+    if (relNum < 0 || relNum >= MAXOPEN) {
+            return RELNUM_OUT_OF_BOUND;
+        }
+
+    char slotmapbytes[4] = { 0 };
+    char page[PAGESIZE] = { 0 };
+
+    if (g_buffer[relNum].dirty == TRUE && g_buffer[relNum].pid != 0) {
+        unsigned int slotmap = g_buffer[relNum].page.slotmap;
+
+        /*convert slotmap to byte array */
+        slotmapbytes[0] = slotmap & 0xFF;
+        slotmapbytes[1] = (slotmap >> 8) & 0xFF;
+        slotmapbytes[2] = (slotmap >> 16) & 0xFF;
+        slotmapbytes[3] = (slotmap >> 24) & 0xFF;
+
+        /* construct a byte array corresponding to page */
+        memcpy(page, slotmapbytes, (PAGESIZE - MAXRECORD));
+        memcpy(page + (PAGESIZE - MAXRECORD), g_buffer[relNum].page.contents, MAXRECORD);
+
+        int offset = (g_buffer[relNum].pid - 1) * PAGESIZE;
+        write_to_file(g_catcache[relNum].relName, g_db_name, offset, page, PAGESIZE);
+    }
+    g_buffer[relNum].dirty = FALSE;
+    g_buffer[relNum].pid = 0;
+
+    return OK;
 }
