29f714b1e5bc77b94f16cd0299abd210f4df0ce5
dheerajram91 <dheerajsaketham@gmail.com>
2014-10-31 23:36:23 +0530

    Implemeted findrelnum

---
+++ include/createcats.h
@@ -0,0 +1,23 @@
+/*
+ * opencats.h
+ *
+ *  Created on: 31-Oct-2014
+ *      Author: Dheeraj
+ */
+
+#ifndef CREATECATS_H_
+#define CREATECATS_H_
+
+#include "error.h"
+#include "defs.h"
+#include "globals.h"
+ 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <dirent.h>
+
+int CreateCats();
+
+#endif
---
+++ include/openrel.h
@@ -0,0 +1,26 @@
+/*
+ * openrel.h
+ *
+ *  Created on: 31-Oct-2014
+ *      Author: Dheeraj
+ */
+
+#ifndef OPENREL_H_
+#define OPENREL_H_
+
+#include "cache.h"
+#include "globals.h"
+#include "readpage.h"
+#include "error.h"
+#include "helpers.h"
+#include "defs.h"
+#include "findrec.h" 
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int OpenRel(char* RelName);
+
+#endif
